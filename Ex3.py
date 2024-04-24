ccb93d683eb864bf86a092ce7d89d1ae78f31092
Leonardo Medeiros <leonardodahoramedeiros@gmail.com>
2024-04-24 18:40:18 -0300

    Novo

---
+++ Ex3.py
@@ -0,0 +1,6 @@
+valor1 =  int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+
+soma = valor1 + valor2
+
+print("A soma de {} mais o valor {} somados dão o valor {}".format(valor1, valor2, soma))
