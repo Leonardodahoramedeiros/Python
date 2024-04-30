654e8d19629f2af04707437c8cd71c07abc03915
Leonardo Medeiros <leonardodahoramedeiros@gmail.com>
2024-04-30 17:39:05 -0300

    Teste

--- Ex20.py
+++ Ex20.py
@@ -2,6 +2,8 @@ Nome = str(input("Digite seu nome: ")).strip()
 
 print("Seu nome em letras maiusculas é {}".format(Nome.upper()))
 print("Seu nome em letrar minusculas é {}".format(Nome.lower()))
-print("Seu nome tem {} letras!".format(len(Nome)))
+print("Seu nome tem {} letras!".format(len(Nome) - Nome.count(' ')))
 
 print("Seu primeiro nome tem {} letras.".format(Nome.find(' ')))
+separa = Nome.split()
+print("Seu primeiro nome é {} e ele tem {} letras".format(separa[0], len(separa[0])))
--- Ex21.py
+++ Ex21.py
@@ -1,1 +1,15 @@
 
+
+Numero = int(input("Digite um número: "))
+
+u = Numero // 1 % 10 
+d = Numero // 10 % 10
+c = Numero // 100 % 10
+m = Numero // 1000 % 10
+
+print("Analisando o formato do número {}".format(Numero))
+print(" {} Unidades!".format(u))
+print(" {} dezenas!".format(d))
+print(" {} centenas!".format(c))
+print(" {} milhar!".format(m))
+
