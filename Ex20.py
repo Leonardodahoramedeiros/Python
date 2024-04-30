9c8d087a811ca5aee66d886fee2ccfa27b429b04
Leonardo Medeiros <leonardodahoramedeiros@gmail.com>
2024-04-30 16:41:46 -0300

    Ex20 atualizado

--- Ex20.py
+++ Ex20.py
@@ -1,1 +1,7 @@
+Nome = str(input("Digite seu nome: ")).strip()
 
+print("Seu nome em letras maiusculas é {}".format(Nome.upper()))
+print("Seu nome em letrar minusculas é {}".format(Nome.lower()))
+print("Seu nome tem {} letras!".format(len(Nome)))
+
+print("Seu primeiro nome tem {} letras.".format(Nome.find(' ')))
