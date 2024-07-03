f1c1cef98b334ff66fcf0ab21be5e1c6f2932e02
Leonardo Medeiros <leonardodahoramedeiros@gmail.com>
2024-07-03 09:45:52 -0300

    Novos alterações

--- Ex71.py
+++ Ex71.py
@@ -1,1 +1,16 @@
+valor = int(input("Quer ver a tabuada de qual valor: "))
 
+print("=-"*20)
+
+contador = 0
+
+while valor > 0:
+
+    for i in range(1, 11):
+        print("{} x {} = {}".format(i, valor, i * valor))
+        contador = contador + 1
+
+    valor = int(input("Quer ver a tabuada de qual valor: "))
+    
+print("=-"*20)
+print("PROGRAMA DE TABUADA ENCERRADO! VOLTE SEMPRE!")
---
+++ Ex72.py
@@ -0,0 +1,12 @@
+from random import randint
+print("=-"*20)
+print("VAMOS JOGAR PAR OU IMPAR")
+print("=-"*20)
+valor = int(input("Digite um valor: "))
+decisao = str(input("Par ou ímpar:[P/I] "))
+
+computador = randint(0, 10)
+
+print(computador)
+
+
